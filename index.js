83c26ac2137c0323aaef614bae511bcc865226c3
Elijha Couch <31853975+ElijhaC@users.noreply.github.com>
2026-09-01 19:07:23 -0400

    Generate random numbers when game start

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+
+function getRandomCard() {
+    let randomNumer = Math.floor( Math.random()*13 ) + 1
+    if (randomNumer > 10) {
+        return 10
+    } else if (randomNumer === 1) {
+        return 11
+    } else {
+        return randomNumer
+    }
+}
+
 function startGame() {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    isAlive = true
+    // Generate two random numbers
+    // Re-assign the cards and sum variables so that the game can start
+    let firstNum = getRandomCard()
+    let secondNum = getRandomCard()
+    cards = [firstNum, secondNum]
+    sum = firstNum + secondNum
+    renderGame()
+}
+
+function renderGame() {
+    //Render all cards
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -26,5 +53,6 @@ function startGame() {
 function newCard(){
     let card = 5
     sum += card
-    startGame()
+    cards.push(card)
+    renderGame()
 }
